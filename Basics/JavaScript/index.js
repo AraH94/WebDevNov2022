5c36efb311ac41e594457705c19c16f4e7bdf08a
AraH94 <arahoedemakers@hotmail.com>
2022-12-20 14:05:36 +0100

    javascript lesson 1

---
+++ Basics/JavaScript/index.js
@@ -0,0 +1,50 @@
+console.log('How you doin?');
+
+// alert('DANGERDANGERDANGER');
+
+console.log(6 + 9);
+console.log('1' + 1);
+
+console.log('hel' + 'lo' + ' world');
+console.log(12 / 4);
+console.log(12 % 5);
+
+console.log(typeof 5);
+console.log(typeof 'hello');
+
+// variabletype variableName = Value/Data
+
+// var fullname = "Jean-Claude Van Damme"
+// var 1234fullname = "Michael Jackson" // Can't start with a number
+// var _a = 'Hello'
+// var FULLNAME = "Michael Jordan" //Verschilt van fullname lowercase
+
+// console.log(Boolean);
+
+// console.log(y, 'before');
+// var y = 5;
+// console.log(y, 'before update');
+// y=10; // update
+// console.log(y, 'after updated value')
+
+// var companyname = 'Microsoft' //declaration of a variable
+// console.log(companyname)
+// companyname = 'Intec'
+// console.log(companyname)
+//Update
+// companyname = 'Oracle' //update variable value
+// console.log(companyname)
+// var companyname = 'The Oracle' //re-declaration
+// console.log(companyname)
+
+// var name = 'Joe'
+// function printName() {
+// 	var name = 'Ara'
+// 	console.log(name)
+// }
+
+// printName()
+
+const token = 'abc123' //unique token/password
+token = '987'
+console.log(token)
