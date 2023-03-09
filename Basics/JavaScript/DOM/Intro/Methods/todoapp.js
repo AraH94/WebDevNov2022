094b664656bb1a9caca6e66e43a4f67b01104188
AraH94 <arahoedemakers@hotmail.com>
2023-03-09 15:21:47 +0100

    todoapp and news

--- Basics/JavaScript/DOM/Intro/Methods/todoapp.js
+++ Basics/JavaScript/DOM/Intro/Methods/todoapp.js
@@ -33,8 +33,87 @@ const addTodo1 = document.querySelector('.btn.btn-danger')
 // querySelector or querySelectorAll (accepts css selector)
 
 
-const [addTodo,deleteAllTodos] = document.querySelectorAll('button')
+const [addTodo, deleteAllTodos] = document.querySelectorAll('button')
 
 const FirstForm = document.querySelector('.card-body > #todo-form')
 
-console.log(FirstForm)
+// console.log(FirstForm)
+
+// Find list-group-item (li tagname)
+const TodoItem = document.querySelector(".list-group-item")
+
+// Clone of list-group-item (li)
+const clonedOfTodoItem1 = TodoItem.cloneNode(true); // cloneNode is to take a copy of an element
+const clonedOfTodoItem2 = TodoItem.cloneNode();
+console.log(clonedOfTodoItem1.hasChildNodes(),
+	clonedOfTodoItem2.hasChildNodes()) // hasChildNodes is to know if there are children or not
+// clonedOfTodoItem1.childNodes[0].textContent='Javascript';
+clonedOfTodoItem1.firstChild.textContent = 'Javascript'; // an other possible way for the first child
+
+console.log(clonedOfTodoItem1.childNodes[0])
+ListOfTodos.appendChild(clonedOfTodoItem1)
+// ListOfTodos.appendChild(clonedOfTodoItem2)
+
+// DOMString
+let parser = new DOMParser(); // build in constructor in js -> parsed from string to node object
+let RealNodeObject = parser.parseFromString(`<li class="list-group-item d-flex justify-content-between">
+Reactjs
+<a href="#" class="delete-item">
+	<i class="fa fa-remove"></i>
+</a>
+</li>`, 'text/html'); // text/html = what data type domstring you want to use 
+
+// alternative way to parse domstring
+// createContextualFragment
+let parser1 = document.createRange().createContextualFragment(`<li class="list-group-item d-flex justify-content-between">
+Nodejs
+<a href="#" class="delete-item">
+	<i class="fa fa-remove"></i>
+</a>
+</li>`, 'text/html')
+
+console.log(RealNodeObject.body.firstChild);
+console.log(parser1);
+
+// append()
+// ListOfTodos.append(RealNodeObject.body.firstChild);
+// ListOfTodos.append(parser1);
+ListOfTodos.append(RealNodeObject.body.firstChild, parser1)
+
+// .style property
+ListOfTodos.lastElementChild.style.backgroundColor = "tomato";
+ListOfTodos.lastElementChild.style.color = "white";
+
+// fontsize
+ListOfTodos.lastElementChild.style.fontSize = "20pt";
+console.log(ListOfTodos.lastElementChild.style);
+
+ListOfTodos.childNodes[ListOfTodos.childNodes.length - 2].style.backgroundColor = "cyan"; // -2 is voorlaatste element en -1 is de laatste element
+
+console.log(ListOfTodos.childNodes.length - 2);
+
+// background-color: salmon; color: white, font-size: 20pt;
+ListOfTodos.childNodes[ListOfTodos.childNodes.length - 3].setAttribute
+	('style', 'background-color: salmon; color: white; font-size: 20pt');
+
+console.log(ListOfTodos.lastElementChild);
+
+// usage: setTimeout(callback, ms_time)
+// 1 second is 1000 miliseconds
+// setTimeOut(function(){
+// })
+
+// setTimeout(() => {
+// 	console.log('Hello world')
+// 	ListOfTodos.firstChild.remove()
+// 	ListOfTodos.lastElementChild.remove()
+// 	ListOfTodos.lastChild.remove()
+// }, 5000);
+
+// usage: setInterval(callback, ms_time)
+setInterval(() => {
+	console.log('This is interval')
+	if (ListOfTodos.children.length > 0) {
+		ListOfTodos.firstChild.remove()
+	}
+}, 2000);
--- Basics/JavaScript/HTTPRequests/HomeExercises/homeexercisefetchapi.js
+++ Basics/JavaScript/HTTPRequests/HomeExercises/homeexercisefetchapi.js
@@ -1,16 +1,38 @@
+// function deleteAllPosts() {
+// console.log('Test delete');
+// 	fetch('http://localhost:3000/database/')
+// 		.then(res => res.json())
+// 		.then(data => {
+// 			data.forEach(comment => {
+// 				console.log(`Delete ${comment.id}`)
+// 				fetch(`http://localhost:3000/database/${comment.id}`, {
+// 					method: 'DELETE'
+// 				})
+// 					.then(() => {
+// 						console.log('All posts are deleted');
+// 					})
+// 			})
+// 		})
+// }
+
+let url = 'http://localhost:3000/database/';
+
 function deleteAllPosts() {
-	// console.log('Test delete');
-	fetch('http://localhost:3000/database/')
-		.then(res => res.json())
+	console.log('Deleting all posts...');
+	//delete one by one
+	fetch(url)
+		.then(response => response.json())
 		.then(data => {
-			data.forEach(comment => {
-				console.log(`Delete ${comment.id}`)
-				fetch(`http://localhost:3000/database/${comment.id}`, {
+			data.forEach(post => {
+				console.log(post, 'post deleted');
+				fetch(url + '/' + post.id, {
 					method: 'DELETE'
 				})
-					.then(() => {
-						console.log('All posts are deleted');
+					.then(response => response.json())
+					.then(data => {
+						console.log(data);
 					})
+					.catch(error => console.log(error))
 			})
 		})
 }
